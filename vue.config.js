2a421a0401b71a1232c9f3ed851d279ee9b5ca74
GokulM <gokulgokul0409@gmail.com>
2025-07-02 14:22:35 +0530

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'Product-web.git'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/E-commerce/'  // This should match your repository name
+    : '/'
 })
